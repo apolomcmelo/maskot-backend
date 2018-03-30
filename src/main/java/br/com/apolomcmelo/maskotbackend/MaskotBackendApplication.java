6f691aaa61ae323846ad7e098f3ba75ed5cfd365
Apolo Mc Melo <apolomelo@gmail.com>
2018-03-30 18:28:03 -0300

    Commit inicial com configuração do projeto

---
+++ src/main/java/br/com/apolomcmelo/maskotbackend/MaskotBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.apolomcmelo.maskotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskotBackendApplication.class, args);
+	}
+}
